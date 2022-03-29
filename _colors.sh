1dd6078a83e7ba2c33390f32d266182de31550b3
Cole Brown <code@brown.dev>
2022-03-29 11:26:49 -0700

    Add text properties (bold, etc).

--- _colors.sh
+++ _colors.sh
@@ -1,5 +1,16 @@
+# TODO: Rename this file - not just for colors anymore.
 
+# TODO: split into smaller functions or leave as a big single?
+
+# TODO: More format codes:
+#   - https://misc.flogisoft.com/bash/tip_colors_and_formatting
+#     - Left off at "8/16 Colors"
 _colors_setup() {
+  # ------------------------------
+  # ANSI Codes
+  # ------------------------------
+
+  # Colors
   ansi_color_reset="\033[00m"
   ansi_color_red="\033[0;31m"
   ansi_color_green="\033[01;32m"
@@ -7,6 +18,29 @@ _colors_setup() {
   ansi_color_blue="\033[01;34m"
   ansi_color_purple="\033[0;35m"
   ansi_color_teal="\033[0;36m"
+
+  # Inverts foreground and background colors.
+  # AKA "reverse"
+  ansi_color_invert="\e[7m"
+
+  # Text properties
+  ansi_format_reset="\e[0m"
+  ansi_format_bold="\e[1m"
+  ansi_format_dim="\e[2m"
+  ansi_format_italic="\e[3m"
+  ansi_format_underline="\e[4m"
+  ansi_format_blink="\e[5m"
+  ansi_format_hidden="\e[8m" # For e.g. passwords.
+  ansi_format_strikethrough="\e[9m"
+
+  ansi_format_bold_reset="\e[21m"
+  ansi_format_dim_reset="\e[22m"
+  ansi_format_italic_reset="\e[23m"
+  ansi_format_underline_reset="\e[24m"
+  ansi_format_blink_reset="\e[25m"
+  ansi_format_hidden_reset="\e[28m"
+  ansi_format_strikethrough_reset="\e[29m"
+
 
   #--------------
   # PS1 Colors: De-uglifying Color Codes
